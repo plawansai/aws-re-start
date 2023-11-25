0cd7fc81de5f5de007b3c753b6cfaf1e775e88e5
plawansai <92997423+plawansai@users.noreply.github.com>
2023-11-25 18:23:16 +0700

    Create 02-numeric-data.py

---
+++ 02-numeric-data.py
@@ -0,0 +1,9 @@
+print("Python has three numeric types: int, float, complex, (and bool)")
+myValue=1
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=3.14
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=5j
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=False
+print(str(myValue) + " is of the data type " + str(type(myValue)))
