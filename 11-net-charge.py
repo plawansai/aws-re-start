c9944effbc77b0c34f769c34ee9a9462fb287d0d
plawansai <92997423+plawansai@users.noreply.github.com>
2023-12-03 22:21:07 +0700

    Create 11-net-charge.py

---
+++ 11-net-charge.py
@@ -0,0 +1,14 @@
+# Assigning variables, lists, and dictionaries
+# Python3.6  
+# Coding: utf-8  
+# Store the human preproinsulin sequence in a variable called preproinsulin:  
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"  
+# Store the remaining sequence elements of human insulin in variables:  
+lsInsulin = "malwmrllpllallalwgpdpaaa"  
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"  
+aInsulin = "giveqcctsicslyqlenycn"  
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"  
+insulin = bInsulin + aInsulin
+pKR = {'y':10.07,'c': 8.18,'k':10.53,'h':6.00,'r':12.48,'d':3.65,'e':4.25}
+
+# Using count() to count the numbers of each amino acid
