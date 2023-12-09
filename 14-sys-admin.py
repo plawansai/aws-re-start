e2b19fa00a72cb9ceb1df4c8ddb793ee5d98a14c
plawansai <92997423+plawansai@users.noreply.github.com>
2023-12-09 22:21:06 +0700

    Create 14-sys-admin.py

---
+++ 14-sys-admin.py
@@ -0,0 +1,11 @@
+# Using os.system
+import os
+os.system("ls")
+
+# Using subprocess.run
+import subprocess
+subprocess.run(["ls"])
+
+# Using subprocess.run with two arguments
+subprocess.run(["ls","-l"])
+
