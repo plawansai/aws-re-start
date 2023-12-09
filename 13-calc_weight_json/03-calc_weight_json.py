caffe12564b409aea7d0f34dc407c848e0407353
plawansai <92997423+plawansai@users.noreply.github.com>
2023-12-09 18:09:00 +0700

    Create 03-calc_weight_json.py

---
+++ 13-calc_weight_json/03-calc_weight_json.py
@@ -0,0 +1,12 @@
+import 02-jsonFileHandler
+data = 02-jsonFileHandler.readJsonFile('files/01-insulin.json')
+if data != "" :
+    bInsulin = data['molecules']['bInsulin']
+    aInsulin = data['molecules']['aInsulin']
+    insulin = bInsulin + aInsulin
+    molecularWeightInsulinActual = data['molecularWeightInsulinActual']
+    print('bInsulin: ' + bInsulin)
+    print('aInsulin: ' + aInsulin)
+    print('molecularWeightInsulinActual: ' + str(molecularWeightInsulinActual))
+else:
+    print("Error. Exiting program")
