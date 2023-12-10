39de626c45224eb284b55e082ae9489964a0d47b
plawansai <92997423+plawansai@users.noreply.github.com>
2023-12-10 19:01:34 +0700

    Update 14-sys-admin.py

--- 14-sys-admin.py
+++ 14-sys-admin.py
@@ -1,21 +1,21 @@
-printf("Using os.system")
+print("Using os.system")
 import os
 os.system("ls")
 
-printf("\nUsing subprocess.run")
+print("\nUsing subprocess.run")
 import subprocess
 subprocess.run(["ls"])
 
-printf("\nUsing subprocess.run with two arguments")
+print("\nUsing subprocess.run with two arguments")
 subprocess.run(["ls","-l"])
 
-printf("\nUsing subprocess.run with three arguments")
+print("\nUsing subprocess.run with three arguments")
 subprocess.run(["ls","-l","README.md"])
 
-# Retrieving system information
+print("\nRetrieving system information")
 command="uname"
 commandArgument="-a"
 print(f'Gathering system information with command: {command} {commandArgument}')
 subprocess.run([command,commandArgument])
 
-# 
+
