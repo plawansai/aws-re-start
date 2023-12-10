7934f6dc3e49f0c3f2e0afc881ad20d255d8920c
plawansai <92997423+plawansai@users.noreply.github.com>
2023-12-10 19:10:10 +0700

    Update 14-sys-admin.py

--- 14-sys-admin.py
+++ 14-sys-admin.py
@@ -1,24 +1,24 @@
-print("Using os.system")
+print("Using os.system:")
 import os
 os.system("ls")
 
-print("\nUsing subprocess.run")
+print("\nUsing subprocess.run:")
 import subprocess
 subprocess.run(["ls"])
 
-print("\nUsing subprocess.run with two arguments")
+print("\nUsing subprocess.run with two arguments:")
 subprocess.run(["ls","-l"])
 
-print("\nUsing subprocess.run with three arguments")
+print("\nUsing subprocess.run with three arguments:")
 subprocess.run(["ls","-l","README.md"])
 
-print("\nRetrieving system information")
+print("\nRetrieving system information:")
 command="uname"
 commandArgument="-a"
 print(f'Gathering system information with command: {command} {commandArgument}')
 subprocess.run([command,commandArgument])
 
-print("\nRetrieving information about disk space")
+print("\nRetrieving information about disk space:")
 command="ps"
 commandArgument="-x"
 print(f'Gathering active process information with command: {command} {commandArgument}')
